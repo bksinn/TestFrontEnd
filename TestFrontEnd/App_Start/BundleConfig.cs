7a9f8a677470073919e777b1d7e4ef9ab7a63ad4
Bharat Singh <bksingh@g.cofc.edu>
2019-06-21 09:12:57 -0400

    Sending to Joe

--- TestFrontEnd/App_Start/BundleConfig.cs
+++ TestFrontEnd/App_Start/BundleConfig.cs
@@ -20,10 +20,10 @@ namespace TestFrontEnd
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                     /* "~/Scripts/bootstrap.js"*/));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      //"~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
